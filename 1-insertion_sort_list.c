3b0c786d2e7793182be1786dbc172eb1a4f41a45
MutisoMuli <mulimutiso525@gmail.com>
2023-12-16 13:32:43 +0300

    Done

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,14 +1,14 @@
 #include "sort.h"
 
 /**
- * exchange_nodes - Exchange two nodes in a doubly-linked list of type listint_t.
+ * exchange_nodes - Exchange two nodes in doubly-linked list of type listint_t.
  * @h: A pointer to the head of the doubly-linked list.
  * @n1: A pointer to the first node to be swapped.
  * @n2: The second node to be swapped.
  */
 void exchange_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	(*n1)-> next = n2->next;
+	(*n1)->next = n2->next;
 	if (n2->next != NULL)
 		n2->next->prev = *n1;
 	n2->prev = (*n1)->prev;
@@ -22,7 +22,8 @@ void exchange_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 }
 
 /**
- * insertion_sort_list - Applies the insertion sort algorithm to sort a doubly linked list of integers.
+ * insertion_sort_list - Applies the insertion sort algorithm to sort a
+ * doubly linked list of integers.
  * @list: A pointer to the head of the doubly-linked list of integers.
  *
  * Description: Prints the list after each swap.
